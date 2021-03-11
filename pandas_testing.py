eb3f32b74af87c1fbb9ca26d2659f7059942b295
jasparkatt <jasparkatt@yahoo.com>
2021-03-10 19:30:27 -0600

    g

--- pandas_testing.py
+++ pandas_testing.py
@@ -19,17 +19,3 @@ print(df)
 # strip the cols names down to bare bones header
 df_cols = [i.split(' ')[1] for i in df.columns]
 df.columns = df_cols
-
-
-
-
-# get that index col into its own col(eventually change to a true date string)
-# df.set_index(df.index, inplace=True)
-# print(df)
-# df.index.rename('date', inplace=True)
-df.index = pd.to_datetime(df.index)
-print(df)
-print(df.dtypes)
-# all cols are an 'object' will need to convert to floats and 'date' in pandas
-# dfn = df.apply(pd.to_numeric)
-
